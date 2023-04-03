e1051682364247ce35564630aae07e7223ec712e
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-04-03 11:12:39 +0530

    setup taiwlind css

--- components/HomePage/About.jsx
+++ components/HomePage/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>img</div>
+      <div>Gunpriya</div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../../styles/Navbar.module.css"
 
 function Navbar() {
   return (
     <div>
-      <div className={styles.navbar_container}>
+      <div>
         {/* <p>🌟Gunpriya&nbsp;</p> */}
-        <div className={styles.navbar}>
+        <div>
           <Link href="/">
-            <div>Gunpriya</div>
+            <small>Gunpriya</small>
           </Link>
           <Link href="/blog">
-            <small className={styles.item}>Blog</small>
+            <small>Blog</small>
           </Link>
           <Link href="/projects">
-            <small className={styles.item}>Projects</small>
+            <small>Projects</small>
           </Link>
           <Link href="/projects">
-            <small className={styles.item}>contacts</small>
+            <small>contacts</small>
           </Link>
         </div>
       </div>
